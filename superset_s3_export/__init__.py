8741d25a7bb101ef52c4940a6aa7404adb2e7de3
batmon <thewandererbg@gmail.com>
2026-01-02 20:54:36 +0700

    Fix export page

--- superset_s3_export/__init__.py
+++ superset_s3_export/__init__.py
@@ -5,7 +5,8 @@ Superset S3 Export Plugin
 __version__ = "0.1.0"
 
 from flask import Flask, render_template
-from flask_appbuilder import BaseView, expose, has_access
+from flask_appbuilder import BaseView, expose
+from flask_appbuilder.security.decorators import has_access_api
 
 
 class S3ExportView(BaseView):
@@ -15,7 +16,7 @@ class S3ExportView(BaseView):
     default_view = "list"
 
     @expose("/")
-    @has_access  # CRITICAL: Required for FAB permissions
+    @has_access_api  # CRITICAL: Required for FAB permissions
     def list(self):
         """Show list of exports for current user"""
         return render_template("s3_export_list.html")
